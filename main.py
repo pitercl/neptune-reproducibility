bc288de9a29420e1d5611ef20dc68a99aa33f22d
Hubert Jaworski <hubert.jaworski@neptune.ml>
2019-06-27 15:09:11 +0200

    v3

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-print("hello")
 import os
-print(os.getenv("NEPTUNE_PROJECT", ""))
-print(os.getenv("NEPTUNE_API_TOKEN", ""))
-print(os.getenv("RUN_COMMAND", ""))
-print(os.getenv("GIT_REPO", ""))
-print(os.getenv("GIT_COMMIT_SHA", ""))
-print(os.getenv("DOCKER_IMAGE", ""))
-print(os.getenv("DOCKER_EXTRA_PARAMS", ""))
-
 import neptune
 
 neptune.init()
 
 experiment = neptune.create_experiment("Repro v2")
+
 experiment.set_property("git_remote", os.getenv("GIT_REPO", ""))
 experiment.set_property("docker_image", os.getenv("DOCKER_IMAGE", ""))
 experiment.set_property("docker_extra_params", os.getenv("DOCKER_EXTRA_PARAMS", ""))
---
+++ neptune_repr.py
@@ -0,0 +1,45 @@
+
+import git
+import subprocess
+import neptune
+import sys
+import os
+
+git_repo = git.Repo('.', search_parent_directories=True)
+git_root = git_repo.git.rev_parse("--show-toplevel")
+
+path = git_root + "/repro.yaml"
+
+project = neptune.init()
+
+exp_name = sys.argv[1]
+
+exps = project.get_experiments(exp_name)
+
+exp = exps[0]
+
+props = exp.get_properties()
+
+cmd = props["cmd"]
+docker_image = props["docker_image"]
+docker_extra_params = props["docker_extra_params"]
+
+git_remote = props["git_remote"]
+git_sha = props["git_sha"]
+
+command = [
+    "docker", "run", "-it",
+    "--entrypoint", "/bin/bash",
+    "-v", os.path.expanduser("~") + "/.ssh:/root/.ssh:ro",
+    "-e", "NEPTUNE_PROJECT=" + project.full_id,
+    "-e", "NEPTUNE_API_TOKEN=" + os.getenv("NEPTUNE_API_TOKEN", ""),
+    "-e", "RUN_COMMAND=" + cmd + "",
+    "-e", "GIT_REPO=" + git_remote + "",
+    "-e", "GIT_COMMIT_SHA=" + git_sha,
+    "-e", "DOCKER_IMAGE=" + docker_image + "",
+    "-e", "DOCKER_EXTRA_PARAMS=" + docker_extra_params,
+    docker_image,
+    "-c", "git clone -q \"$GIT_REPO\" /repo && cd /repo && git checkout \"$GIT_COMMIT_SHA\" && $RUN_COMMAND"
+]
+
+subprocess.Popen(command, shell=False).communicate()
